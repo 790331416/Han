2bcfc490f973a89862b12f580cacd782d72918a9
薛朝元 <790331416@qq.com>
2026-05-15 13:14:25 +0800

    fix: guard dept leader legacy column

--- sql/upgrades/postgres/phase10_sys_dept_leader_id_compat.sql
+++ sql/upgrades/postgres/phase10_sys_dept_leader_id_compat.sql
@@ -1,9 +1,20 @@
 ALTER TABLE sys_dept
     ADD COLUMN IF NOT EXISTS leader_id BIGINT;
 
-UPDATE sys_dept
-SET leader_id = leader::BIGINT
-WHERE leader_id IS NULL
-  AND leader IS NOT NULL
-  AND btrim(leader) <> ''
-  AND leader ~ '^[0-9]+$';
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_schema = 'public'
+          AND table_name = 'sys_dept'
+          AND column_name = 'leader'
+    ) THEN
+        UPDATE sys_dept
+        SET leader_id = leader::BIGINT
+        WHERE leader_id IS NULL
+          AND leader IS NOT NULL
+          AND btrim(leader) <> ''
+          AND leader ~ '^[0-9]+$';
+    END IF;
+END $$;
