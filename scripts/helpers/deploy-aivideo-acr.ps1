a0933dd6ae84b215d90d47f062bddce1c5a204cb
薛朝元 <790331416@qq.com>
2026-06-16 16:08:55 +0800

    fix(deploy): support powershell utf8 encoding ctor

--- scripts/helpers/deploy-aivideo-acr.ps1
+++ scripts/helpers/deploy-aivideo-acr.ps1
@@ -228,7 +228,7 @@ $remoteCommand = "TAG='$Tag' REGISTRY='$Registry' DEPLOY_DIR='$DeployDir' SERVIC
 $tempRemoteScript = New-TemporaryFile
 try {
     # PowerShell 管道给 ssh 传字符串时可能带入 BOM，导致远端 bash 的 set -e 失效。
-    $utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+    $utf8NoBom = New-Object System.Text.UTF8Encoding -ArgumentList $false
     [System.IO.File]::WriteAllText($tempRemoteScript.FullName, $remoteScript, $utf8NoBom)
     $process = Start-Process -FilePath 'ssh' `
         -ArgumentList @($SshTarget, $remoteCommand) `
